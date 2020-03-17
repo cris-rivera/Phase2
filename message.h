54317151917073409b568080831167e3e91ac982
Cris Rivera <cris.rivera131@gmail.com>
2020-03-17 11:35:28 -0400

    cleaned up some code in message.h

--- message.h
+++ message.h
@@ -14,7 +14,7 @@ struct mbox_proc {
   int            pid;
   int              mbox_id; //to keep track of which mailbox caused the process  to block.
   int              status;
-  mbox_proc_ptr    next_mbox_ptr; //used for BlockedList, I think.
+  mbox_proc_ptr    next_mbox_ptr; //used for BlockedList.
 };
 
 struct mailbox {
@@ -24,7 +24,6 @@ struct mailbox {
    int              num_slots;   //number of slots mailbox holds
    int              slot_size;  //size of each mailbox slot
    int              status;     //to tell if the mailbox has been/is being released.
-   /* other items as needed... */
 };
 
 struct mail_slot {
@@ -34,7 +33,6 @@ struct mail_slot {
    slot_ptr  next_slot;
    char      message[MAX_MESSAGE];
    int       m_size; //size of the message copied
-   /* other items as needed... */
 };
 
 enum {
